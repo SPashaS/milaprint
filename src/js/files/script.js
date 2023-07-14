d337df238146ae14fd3d0868314ffac2e11dceb6
SPashaS <SPavel.LD@gmail.com>
2023-07-14 20:47:37 +0300

    init: initial commit

---
+++ src/js/files/script.js
@@ -0,0 +1,13 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+
+// const link = document.querySelector(".menu-item.menu-item_has-children");
+
+// console.dir(link);
+
+// link.addEventListener("mouseover", () => {
+//   link.children[0].classList.add("_hover");
+// })
